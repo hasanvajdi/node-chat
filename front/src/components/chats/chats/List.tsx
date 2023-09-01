707419c713430a8db6bde012c0bc7271ece4bcec
hasanvajdi <hasanvajdi9@gmail.com>
2023-09-01 17:19:15 +0330

    refactor: remove extral code from chats List component

--- front/src/components/chats/chats/List.tsx
+++ front/src/components/chats/chats/List.tsx
@@ -1,18 +1,14 @@
-import { useEffect, useState, memo } from "react";
-
 //  tyeps and interfaces
 import { chatItemType } from "./types";
 import { Divider } from "antd";
 //  hooks
 import { useGetChatsQuery } from "redux/requests/chats";
 //  components
-import CreateChat from "./CreateModal";
 import Item from "./Item";
 
 function List() {
   //  hooks
   const chats = useGetChatsQuery(null);
-  console.log("list rendered");
 
   //  handlers
   const handleShowChats = () => {
