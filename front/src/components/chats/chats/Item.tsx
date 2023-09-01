6a2afdd7ca6bfc390ad4f8b504c53ce28a81b86c
hasanvajdi <hasanvajdi9@gmail.com>
2023-09-01 16:38:26 +0330

    style: set skeleton for chat avatar

--- front/src/components/chats/chats/Item.tsx
+++ front/src/components/chats/chats/Item.tsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
+
 //  packages
 import { AvatarGenerator } from "random-avatar-generator";
+import { Skeleton } from "antd";
 
 //  styles
 import chatItemStyles from "./styles.module.scss";
 
 function Item(props: any) {
+  //  variables
   const { data } = props;
   const generator = new AvatarGenerator();
-  console.log("aaaa : ");
+
+  //  states
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
 
   return (
     <div className={chatItemStyles.chatItemContainer}>
       <div className={chatItemStyles.chatItemAvatar}>
-        <img src={generator.generateRandomAvatar()} alt="avatar" />
+        <img
+          src={generator.generateRandomAvatar()}
+          alt="avatar"
+          onLoad={() => setImageIsLoaded(true)}
+        />
+        {!imageIsLoaded && (
+          <Skeleton.Avatar
+            active
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              height: "100%",
+              width: "100%",
+            }}
+          />
+        )}
       </div>
       <span className={chatItemStyles.chatTitle}>{data.user2}</span>
     </div>
--- front/src/components/chats/chats/index.tsx
+++ front/src/components/chats/chats/index.tsx
@@ -10,6 +10,7 @@ import Item from "./Item";
 
 //  tyeps and interfaces
 import { chatItemType } from "./types";
+import { Divider } from "antd";
 
 function Chats() {
   //  hooks
@@ -26,7 +27,12 @@ function Chats() {
       const data: chatItemType[] = chats.data as chatItemType[];
 
       if (data.length) {
-        return data.map((item: chatItemType) => <Item data={item} />);
+        return data.map((item: chatItemType) => (
+          <>
+            <Item data={item} />
+            <Divider style={{ marginTop: 5, marginBottom: 5, height: 0 }} />
+          </>
+        ));
       } else {
         return <span>no chat yet</span>;
       }
